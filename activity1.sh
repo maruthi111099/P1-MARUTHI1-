ba96c685d67313ccb7cfe8957f515bff1be3b820
Maruthi B M <120936649+maruthi111099@users.noreply.github.com>
2022-12-24 17:16:34 +0530

    Add files via upload

---
+++ activity1.sh
@@ -0,0 +1,6 @@
+echo "Welcome User! "
+read -p "Enter Your Firstname : " firstName
+read -p "Enter Your Lastname : " lastName
+read -p "Enter Your Date-of-birth in dd/mm/yy format : " dob
+echo -e "\nName - $firstName $lastName"
+echo "Date of Birth - $dob"
